6778019e47a6cfaf1e4c28e0c14ecd3e6ce86e7b
vsavkin <avix1000@gmail.com>
2016-07-31 11:23:58 -0700

    set up debugging env

--- src/treeshaker.ts
+++ src/treeshaker.ts
@@ -1,3 +1,21 @@
-export function run(entry: string): string {
+import * as path from 'path';
+import * as ts from 'typescript';
+
+const baseTsOptions: ts.CompilerOptions = {
+  moduleResolution: ts.ModuleResolutionKind.Classic
+};
+
+export function run(entrypoint: string): string {
+  const host = ts.createCompilerHost(baseTsOptions);
+  const normalizedEntryPoint = path.normalize(entrypoint);
+
+  if (!entrypoint.match(/\.ts$/)) {
+    throw new Error(`Source file "${entrypoint}" is not a TypeScript file`);
+  }
+
+  const program = ts.createProgram([entrypoint], baseTsOptions, host);
+
+  console.log(program);
+
   return "fixed";
 }
